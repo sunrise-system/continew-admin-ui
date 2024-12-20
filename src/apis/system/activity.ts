ab91f44d9955695e78f35bc3773a8b1d2d1021d0
sunriseyuen <tiptoes@foxmail.com>
2024-12-20 20:25:24 +0800

    Update activity.ts

--- src/apis/system/activity.ts
+++ src/apis/system/activity.ts
@@ -3,6 +3,9 @@ import http from '@/utils/http'
 const BASE_URL = '/activity'
 
 /** @desc 行为/动作 */
-export function fnMotion(sUrl: string, data: any) {
+export function fnMotion(sUrl: string, formArray: any) {
+  let data={}
+  data.data=formArray;
+
   return http.post(`${BASE_URL}${sUrl}`, data)
 }
