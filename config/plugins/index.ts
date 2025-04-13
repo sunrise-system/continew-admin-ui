e4eacb2622836214126a6f60e13d5001f704e19d
sunriseyuen <tiptoes@foxmail.com>
2025-04-13 08:55:50 +0800

    Update index.ts

--- config/plugins/index.ts
+++ config/plugins/index.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import appInfo from './app-info'
 import createDevtools from './devtools'
 import createAutoImport from './auto-import'
@@ -18,5 +21,22 @@ export default function createVitePlugins(viteEnv, isBuild = false) {
   vitePlugins.push(createComponents())
   vitePlugins.push(createSvgIcon(isBuild))
   vitePlugins.push(createMock(viteEnv, isBuild))
+  vitePlugins.push(AutoImport({
+    imports: [
+      'vue',
+      {
+        'naive-ui': [
+          'useDialog',
+          'useMessage',
+          'useNotification',
+          'useLoadingBar'
+        ]
+      }
+    ]
+  }))
+  vitePlugins.push(Components({
+    resolvers: [NaiveUiResolver()]
+  }))
+
   return vitePlugins
 }
