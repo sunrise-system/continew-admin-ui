76ecfc918b6917bbe9853a5568a61815d50d89b1
sunriseyuen <tiptoes@foxmail.com>
2025-04-22 09:27:35 +0800

    Update enum.ts

--- src/apis/system/enum.ts
+++ src/apis/system/enum.ts
@@ -6,30 +6,37 @@ export type * from './type'
 const BASE_URL = '/system/enum'
 
 /** @desc 查询字典列表 */
-export function listEnum(query?: T.DictQuery) {
+export function listEnum(query?: T.EnumQuery) {
   return fetchMotion(`ADM08919A/list`, query)
-
-  // return http.get<T.DictResp[]>(`${BASE_URL}/list`, query)
 }
 
 /** @desc 查询字典详情 */
 export function getEnum(id: string) {
-  return http.get<T.DictResp>(`${BASE_URL}/${id}`)
+  const formArray: any = {}
+  formArray.id = id
+  return fetchMotion(`ADM08919A`, formArray)
 }
 
 /** @desc 新增字典 */
 export function addEnum(data: any) {
-  return http.post(`${BASE_URL}`, data)
+  const formArray: any = {}
+  formArray.data = data
+  return fetchMotion(`ADM08919A/savechanges`, formArray)
 }
 
 /** @desc 修改字典 */
 export function updateEnum(data: any, id: string) {
-  return http.put(`${BASE_URL}/${id}`, data)
+  const formArray: any = {}
+  formArray.data = data
+  formArray.id = id
+  return fetchMotion(`ADM08919A/savechanges`, formArray)
 }
 
 /** @desc 删除字典 */
 export function deleteEnum(id: string) {
-  return http.del(`${BASE_URL}/${id}`)
+  const formArray: any = {}
+  formArray.id = id
+  return fetchMotion(`ADM08919A/delete`, formArray)
 }
 
 /** @desc 清除字典缓存 */
@@ -38,26 +45,37 @@ export function clearEnumCache(code: string) {
 }
 
 /** @desc 查询字典项列表 */
-export function listEnumItem(query: T.DictItemPageQuery) {
-  return http.get<PageRes<T.DictItemResp[]>>(`${BASE_URL}/item`, query)
+export function listEnumItem(query: T.EnumItemPageQuery) {
+  return fetchMotion(`ADM08920A/list`, query)
+
+  // return http.get<PageRes<T.DictItemResp[]>>(`${BASE_URL}/item`, query)
 }
 
 /** @desc 查询字典项详情 */
 export function getEnumItem(id: string) {
-  return http.get<T.DictItemResp>(`${BASE_URL}/item/${id}`)
+  const formArray: any = {}
+  formArray.id = id
+  return fetchMotion(`ADM08920A`, formArray)
 }
 
 /** @desc 新增字典项 */
 export function addEnumItem(data: any) {
-  return http.post(`${BASE_URL}/item`, data)
+  const formArray: any = {}
+  formArray.data = data
+  return fetchMotion(`ADM08920A/savechanges`, formArray)
 }
 
 /** @desc 修改字典项 */
 export function updateEnumItem(data: any, id: string) {
-  return http.put(`${BASE_URL}/item/${id}`, data)
+  const formArray: any = {}
+  formArray.data = data
+  formArray.id = id
+  return fetchMotion(`ADM08920A/savechanges`, formArray)
 }
 
 /** @desc 删除字典项 */
 export function deleteEnumItem(id: string) {
-  return http.del(`${BASE_URL}/item/${id}`)
+  const formArray: any = {}
+  formArray.id = id
+  return fetchMotion(`ADM08920A/delete`, formArray)
 }
