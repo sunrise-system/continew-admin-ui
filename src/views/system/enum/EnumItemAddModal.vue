1ba19da51e80236ae1e27fc65c1f1b9cdb5bf835
sunriseyuen <tiptoes@foxmail.com>
2025-04-26 13:20:40 +0800

    Update EnumItemAddModal.vue

--- src/views/system/enum/EnumItemAddModal.vue
+++ src/views/system/enum/EnumItemAddModal.vue
@@ -9,20 +9,8 @@
     @before-ok="save"
     @close="reset"
   >
+  {{ categoryId }}
     <GiForm ref="formRef" v-model="form" :columns="columns">
-      <template #color>
-        <a-select
-          v-model="form.color"
-          placeholder="请选择颜色"
-          allow-clear
-        >
-          <a-option value="primary"><a-tag color="arcoblue">主要（极致蓝）</a-tag></a-option>
-          <a-option value="success"><a-tag color="green">成功（仙野绿）</a-tag></a-option>
-          <a-option value="warning"><a-tag color="orangered">警告（活力橙）</a-tag></a-option>
-          <a-option value="error"><a-tag color="red">错误（浪漫红）</a-tag></a-option>
-          <a-option value="default"><a-tag color="gray">默认（中性灰）</a-tag></a-option>
-        </a-select>
-      </template>
     </GiForm>
   </a-modal>
 </template>
@@ -74,12 +62,6 @@ const columns: ColumnItem[] = reactive([
     props: {
       maxLength: 30,
     },
-  },
-  {
-    label: '颜色',
-    field: 'color',
-    type: 'input',
-    span: 24,
   },
   {
     label: '排序',
