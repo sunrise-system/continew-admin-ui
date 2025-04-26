47c63ea66f9dde61021f6b46803c4473c6cb4948
sunriseyuen <tiptoes@foxmail.com>
2025-04-26 10:05:45 +0800

    Update client.ts

--- src/apis/system/client.ts
+++ src/apis/system/client.ts
@@ -1,31 +1,38 @@
 import type * as T from './type'
-import http from '@/utils/http'
+import http, { fetchMotion } from '@/utils/http'
 
 export type * from './type'
 
-const BASE_URL = '/system/client'
-
 /** @desc 查询客户端列表 */
 export function listClient(query: T.ClientPageQuery) {
-  return http.get<PageRes<T.ClientResp[]>>(`${BASE_URL}`, query)
+  return fetchMotion(`ADM08921A/list`, query)
 }
 
 /** @desc 查询客户端详情 */
 export function getClient(id: string) {
-  return http.get<T.ClientDetailResp>(`${BASE_URL}/${id}`)
+  const formArray: any = {}
+  formArray.id = id
+  return fetchMotion(`ADM08921A`, formArray)
 }
 
 /** @desc 新增客户端 */
 export function addClient(data: any) {
-  return http.post(`${BASE_URL}`, data)
+  const formArray: any = {}
+  formArray.data = data
+  return fetchMotion(`ADM08921A/savechanges`, formArray)
 }
 
 /** @desc 修改客户端 */
 export function updateClient(data: any, id: string) {
-  return http.put(`${BASE_URL}/${id}`, data)
+  const formArray: any = {}
+  formArray.data = data
+  formArray.id = id
+  return fetchMotion(`ADM08921A/savechanges`, formArray)
 }
 
 /** @desc 删除客户端 */
 export function deleteClient(id: string) {
-  return http.del(`${BASE_URL}`, { ids: [id] })
+  const formArray: any = {}
+  formArray.id = id
+  return fetchMotion(`ADM08921A/delete`, formArray)
 }
